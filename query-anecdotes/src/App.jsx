99184c08f0b81a72025f8646b8cdae2327d27ea4
Lothrin <m.erkam.arafat@gmail.com>
2024-12-13 11:35:56 +0100

    6.21 Anecdotes with QueryLib

--- query-anecdotes/src/App.jsx
+++ query-anecdotes/src/App.jsx
@@ -1,7 +1,7 @@
-import { useQuery } from "@tanstack/react-query";
-import axios from "axios";
+import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
 import AnecdoteForm from "./components/AnecdoteForm";
 import Notification from "./components/Notification";
+import { getAnecdotes } from "./requests";
 
 const App = () => {
   const handleVote = (anecdote) => {
@@ -10,8 +10,8 @@ const App = () => {
 
   const result = useQuery({
     queryKey: ["anecdotes"],
-    queryFn: () =>
-      axios.get("http://localhost:3001/anecdotes").then((res) => res.data),
+    queryFn: getAnecdotes,
+    refetchOnWindowFocus: false,
   });
   console.log(JSON.parse(JSON.stringify(result)));
 
