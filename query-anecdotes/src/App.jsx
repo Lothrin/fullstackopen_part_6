8f44a429dc8946f99f3fa7097cf22c4f6754605c
Lothrin <m.erkam.arafat@gmail.com>
2024-12-13 11:41:26 +0100

    6.22 Anecdotes with QueryLib

--- query-anecdotes/src/App.jsx
+++ query-anecdotes/src/App.jsx
@@ -1,11 +1,19 @@
 import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
 import AnecdoteForm from "./components/AnecdoteForm";
 import Notification from "./components/Notification";
-import { getAnecdotes } from "./requests";
+import { getAnecdotes, updateNote } from "./requests";
 
 const App = () => {
+  const queryClient = useQueryClient();
+
+  const updateAnecdoteMutation = useMutation({
+    mutationFn: updateNote,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["anecdotes"] });
+    },
+  });
   const handleVote = (anecdote) => {
-    console.log("vote");
+    updateAnecdoteMutation.mutate({ ...anecdote, votes: anecdote.votes + 1 });
   };
 
   const result = useQuery({
